fb7d93e6020b01655d29513f0254c7a14f6148d7
Md Abdul Munim <abdulmunim.buet@gmail.com>
2020-02-01 13:36:48 +0800

    Fix test setup
    # Conflicts:
    #	src/components/scroll.to.stick/scroll.to.stick.js

---
+++ src/components/scroll.to.stick/scroll.to.stick.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import { SCROLL_DIRECTIONS, ScrollHelper } from '../../utils/scroll.helper/scroll.helper';
+
+function ScrollToStick({ applyOn, offset, children }) {
+  const style = {
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    left: 0,
+    transition: 'top 100ms ease-in',
+  };
+
+  const scrollHelper = new ScrollHelper();
+
+  /* eslint-disable no-param-reassign */
+  const onScrollChange = (elem) => {
+    return (scrollInfo) => {
+      const { direction, lastKnownDirection } = scrollInfo;
+      if (direction !== lastKnownDirection) {
+        elem.style.top = direction === 'down' ? `-${offset}px` : 0;
+      }
+    };
+  };
+  /* eslint-enable no-param-reassign */
+
+  useEffect(() => {
+    const applyOnElem = applyOn.current;
+    Object.assign(applyOnElem.style, style);
+
+    scrollHelper.attachHook(onScrollChange(applyOnElem));
+
+    return () => {
+      scrollHelper.decommission();
+    };
+  });
+
+  return <>{children}</>;
+}
+
+ScrollToStick.propTypes = {
+  applyOn: PropTypes.oneOfType(React.Ref).isRequired,
+  offset: PropTypes.number.isRequired,
+  children: PropTypes.oneOfType(React.Component).isRequired,
+};
+
+export default ScrollToStick;
